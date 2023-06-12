cebf97e2293ff22dd8cac17243a46af45175f809
Johnwang <s13602507586@gmail.com>
2023-06-12 17:04:15 +0800

    add TypeInference and UnionType

---
+++ src/TypeInference&UnionType.ts
@@ -0,0 +1,11 @@
+// Type Inference
+let message = "Hello, World!"; // TypeScript 推斷型別為 string
+let age = 30; // TypeScript 推斷型別為 number
+let isEnabled = true; // TypeScript 推斷型別為 boolean
+let someVariable; // TypeScript 推斷型別為 any
+
+// Union Type
+let unionType: string | number | boolean;
+unionType = "Hello, World!";
+unionType = 30;
+unionType = true;
