0398d5eaa8e9ce6603675843b646d1f785b2e1d9
Johnwang <s13602507586@gmail.com>
2023-06-14 15:34:42 +0800

    add array

---
+++ src/array.ts
@@ -0,0 +1,5 @@
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ["John", "Bob", "Alice"];
+
+let numbers2: Array<number> = [1, 2, 3, 4, 5];
+let names2: Array<string> = ["John", "Bob", "Alice"];
